69e114d3559beceb46b8bc34059e403f93484eff
Piyush Rai <piyush.rai@in.nspglobaltech.com>
2026-03-28 13:58:40 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable} suppressHydrationWarning>
-      <body>
+      <body suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
